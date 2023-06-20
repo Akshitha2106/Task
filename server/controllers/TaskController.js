447c5bd0240261a5313671f3ef986657ee05b898
Akshitha2106 <akshithamittapalli@gmail.com>
2023-06-20 23:05:23 +0530

    corrected the errors

--- server/controllers/TaskController.js
+++ server/controllers/TaskController.js
@@ -2,13 +2,14 @@ import { validationResult } from "express-validator"
 import { jsonGenerate } from "../utils/helpers.js";
 import { StatusCode } from "../utils/constants.js";
 import Tasks from "../models/Tasks.js";
+import User from "../models/User.js";
 
 export const createTask = async(req,res) =>{
    const error=validationResult(req);
    if(!error.isEmpty()){
        return res.json(jsonGenerate
           (StatusCode.VALIDATION_ERROR,
-             "Todo is required",
+             "Task is required",
              error.mapped()
           )
         );
@@ -17,12 +18,13 @@ export const createTask = async(req,res) =>{
     const result = await Tasks.create({
         userId:req.userId,
         desc:req.body.desc,
+        title:req.body.title,
 
     })
     if(result){
         const user=await User.findOneAndUpdate({_id:req.userId},
             {
-               $push:{taks:result}
+               $push:{Tasks:result}
             });
             return res.json(jsonGenerate(StatusCode.SUCCESS,"Task created successfully",result));
     }
